3cc6ada180d96b0faf9dffaa88332e37ec9d4bc7
Kyle <kyleday@thinkgeo.com>
2020-05-18 10:45:44 -0500

    thinkgeo/public/above-the-fold#323
    Add placeholder UserControls and prefilled categories in samples.json

---
+++ samples/wpf/HowDoISample/Samples/SettingMapExtent/Placeholder.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ThinkGeo.UI.Wpf;
+
+namespace ThinkGeo.UI.Wpf.HowDoI.SettingMapExtent
+{
+    /// <summary>
+    /// Interaction logic for Placeholder.xaml
+    /// </summary>
+    public partial class Placeholder : UserControl
+    {
+        public Placeholder()
+        {
+            InitializeComponent();
+        }
+
+        private void MapView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ samples/wpf/HowDoISample/Samples/TopologicalValidation/Placeholder.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ThinkGeo.UI.Wpf;
+
+namespace ThinkGeo.UI.Wpf.HowDoI.TopologicalValidation
+{
+    /// <summary>
+    /// Interaction logic for Placeholder.xaml
+    /// </summary>
+    public partial class Placeholder : UserControl
+    {
+        public Placeholder()
+        {
+            InitializeComponent();
+        }
+
+        private void MapView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
