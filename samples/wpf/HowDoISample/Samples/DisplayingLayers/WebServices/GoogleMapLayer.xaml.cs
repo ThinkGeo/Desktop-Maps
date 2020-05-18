005ff80df7bc3aac666074c5f9a233c8319a438b
JoshuaRehagen <joshrehagen@thinkGeo.com>
2020-05-18 14:50:22 -0500

    thinkgeo/public/above-the-fold#326 Made all the Displaying layers samples.

---
+++ samples/wpf/HowDoISample/Samples/DisplayingLayers/WebServices/GoogleMapLayer.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ThinkGeo.UI.Wpf;
+
+namespace ThinkGeo.UI.Wpf.HowDoI
+{
+    /// <summary>
+    /// Interaction logic for Placeholder.xaml
+    /// </summary>
+    public partial class GoogleMapLayer : UserControl
+    {
+        public GoogleMapLayer()
+        {
+            InitializeComponent();
+        }
+
+        private void MapView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ samples/wpf/HowDoISample/Samples/DisplayingLayers/WebServices/NOAAWeatherWarningLayer.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ThinkGeo.UI.Wpf;
+
+namespace ThinkGeo.UI.Wpf.HowDoI
+{
+    /// <summary>
+    /// Interaction logic for Placeholder.xaml
+    /// </summary>
+    public partial class NOAAWeatherWarningLayer : UserControl
+    {
+        public NOAAWeatherWarningLayer()
+        {
+            InitializeComponent();
+        }
+
+        private void MapView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
