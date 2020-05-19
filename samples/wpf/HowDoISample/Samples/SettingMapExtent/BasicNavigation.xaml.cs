5ac384ba74903b8daa7d261ae422daf37795d630
Kyle <kyleday@thinkgeo.com>
2020-05-19 16:27:19 -0500

    thinkgeo/public/above-the-fold#325 improved controls layout

--- samples/wpf/HowDoISample/Samples/SettingMapExtent/BasicNavigation.xaml.cs
+++ samples/wpf/HowDoISample/Samples/SettingMapExtent/BasicNavigation.xaml.cs
@@ -21,15 +21,32 @@ namespace ThinkGeo.UI.Wpf.HowDoI
         /// </summary>
         private void MapView_Loaded(object sender, RoutedEventArgs e)
         {
+            mapView.MapUnit = GeographyUnit.Meter;
+            mapView.ZoomLevelSet = new ThinkGeoCloudMapsZoomLevelSet();
+
             var thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay("itZGOI8oafZwmtxP-XGiMvfWJPPc-dX35DmESmLlQIU~", "bcaCzPpmOG6le2pUz5EAaEKYI-KSMny_WxEAe7gMNQgGeN9sqL12OA~~", ThinkGeoCloudVectorMapsMapType.Light);
             mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
+            mapView.ZoomToScale(mapView.ZoomLevelSet.ZoomLevel03.Scale);
         }
 
         private void Pan_Click(object sender, RoutedEventArgs e)
         {
-            Enum.TryParse(((Button)sender).Name, out PanDirection direction);
-            int.TryParse(Strength.Text, out int strength);
-            mapView.Pan(direction, strength);
+            var percentage = (int)panPercentage.Value;
+            switch (((Button)sender).Name)
+            {
+                case "panNorth":
+                    mapView.Pan(PanDirection.Up, percentage);
+                    break;
+                case "panEast":
+                    mapView.Pan(PanDirection.Right, percentage);
+                    break;
+                case "panWest":
+                    mapView.Pan(PanDirection.Left, percentage);
+                    break;
+                case "panSouth":
+                    mapView.Pan(PanDirection.Down, percentage);
+                    break;
+            }
         }
     }
 }
