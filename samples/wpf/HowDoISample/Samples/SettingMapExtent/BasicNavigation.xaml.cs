d2d4b2b7a50ba229dc06531fd4c6c9e44c20fc57
Kyle <kyleday@thinkgeo.com>
2020-05-20 19:20:32 -0500

    thinkgeo/public/above-the-fold#325
    combine rotate with basic navigation
    begin setmapextent sample

--- samples/wpf/HowDoISample/Samples/SettingMapExtent/BasicNavigation.xaml.cs
+++ samples/wpf/HowDoISample/Samples/SettingMapExtent/BasicNavigation.xaml.cs
@@ -7,7 +7,7 @@ using ThinkGeo.UI.Wpf;
 namespace ThinkGeo.UI.Wpf.HowDoI
 {
     /// <summary>
-    /// This sample demonstrates how to programmatically pan and zoom the map control. 
+    /// Learn how to programmatically zoom, pan, and rotate the map control.
     /// </summary>
     public partial class BasicNavigation : UserControl
     {
@@ -21,15 +21,40 @@ namespace ThinkGeo.UI.Wpf.HowDoI
         /// </summary>
         private void MapView_Loaded(object sender, RoutedEventArgs e)
         {
+            // Set the map's unit of measurement to meters(Spherical Mercator)
             mapView.MapUnit = GeographyUnit.Meter;
-            mapView.ZoomLevelSet = new ThinkGeoCloudMapsZoomLevelSet();
 
+            // Add Cloud Maps as a background overlay
             var thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay("itZGOI8oafZwmtxP-XGiMvfWJPPc-dX35DmESmLlQIU~", "bcaCzPpmOG6le2pUz5EAaEKYI-KSMny_WxEAe7gMNQgGeN9sqL12OA~~", ThinkGeoCloudVectorMapsMapType.Light);
             mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
-            mapView.ZoomToScale(mapView.ZoomLevelSet.ZoomLevel03.Scale);
+
+            // Set the map extent
+            mapView.CurrentExtent = new RectangleShape(-10786436, 3918518, -10769429, 3906002);
         }
 
-        private void Pan_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Zoom in on the map
+        /// The same effect can be achieved by using the ZoomPanBar bar on the upper left of the map, double left clicking on the map, or by using the the scroll wheel.
+        /// </summary>
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            mapView.ZoomIn();
+        }
+
+        /// <summary>
+        /// Zoom out on the map
+        /// The same effect can be achieved by using the ZoomPanBar bar on the upper left of the map, double right clicking on the map, or by using the the scroll wheel.
+        /// </summary>
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            mapView.ZoomOut();
+        }
+
+        /// <summary>
+        /// Pan the map in a direction using the PanDirection enum and set how far to pan based on percentage.
+        /// The same effect can be achieved by using the ZoomPanBar arrows on the upper left of the map or by left click dragging anywhere on the map.
+        /// </summary>
+        private void PanArrow_Click(object sender, RoutedEventArgs e)
         {
             var percentage = (int)panPercentage.Value;
             switch (((Button)sender).Name)
@@ -47,6 +72,16 @@ namespace ThinkGeo.UI.Wpf.HowDoI
                     mapView.Pan(PanDirection.Down, percentage);
                     break;
             }
+        }
+
+        /// <summary>
+        /// Rotate the map at an angle using the value of the rotateAngle Slider. Since this is just setting a property, you must refresh the map in order for the rotation to show.
+        /// The same effect can be achieved by holding down the ALT key and left click dragging anywhere on the map.
+        /// </summary>
+        private void Rotate_Click(object sender, RoutedEventArgs e)
+        {
+            mapView.RotatedAngle = (float)rotateAngle.Value;
+            mapView.Refresh();
         }
     }
 }
--- samples/wpf/HowDoISample/Samples/SettingMapExtent/SetMapExtent.xaml.cs
+++ samples/wpf/HowDoISample/Samples/SettingMapExtent/SetMapExtent.xaml.cs
@@ -7,25 +7,71 @@ using ThinkGeo.UI.Wpf;
 namespace ThinkGeo.UI.Wpf.HowDoI
 {
     /// <summary>
-    /// Interaction logic for Placeholder.xaml
+    /// Learn how to set the map extent using a variety of different methods.
     /// </summary>
     public partial class SetMapExtent : UserControl
     {
+        ShapeFileFeatureLayer friscoCityBoundary;
+
         public SetMapExtent()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Setup the map with the ThinkGeo Cloud Maps overlay to show a basic map and a shapefile with simple data to work with
+        /// </summary>
         private void MapView_Loaded(object sender, RoutedEventArgs e)
         {
+            // Set the map's unit of measurement to meters(Spherical Mercator)
+            mapView.MapUnit = GeographyUnit.Meter;
+
+            // Add Cloud Maps as a background overlay
             var thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay("itZGOI8oafZwmtxP-XGiMvfWJPPc-dX35DmESmLlQIU~", "bcaCzPpmOG6le2pUz5EAaEKYI-KSMny_WxEAe7gMNQgGeN9sqL12OA~~", ThinkGeoCloudVectorMapsMapType.Light);
             mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
+
+            // Load the Frisco data to a layer
+            friscoCityBoundary = new ShapeFileFeatureLayer(@"../../../Data/FriscoMunBnd/City_ETJ.shp");
+
+            // Convert the Frisco shapefile from its native projection to Spherical Mercator, to match the map
+            friscoCityBoundary.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
+
+            // Style the data so that we can see it on the map
+            friscoCityBoundary.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyle.CreateSimpleAreaStyle(new GeoColor(16, GeoColors.Blue), GeoColors.DimGray, 2);
+            friscoCityBoundary.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            // Add Frisco data to a LayerOverlay and add it to the map
+            var layerOverlay = new LayerOverlay();
+            layerOverlay.Layers.Add(friscoCityBoundary);
+            mapView.Overlays.Add(layerOverlay);
+
+            // Set the map extent
+            mapView.CurrentExtent = new RectangleShape(-10786436, 3918518, -10769429, 3906002);
         }
 
-        private void panUp_Click(object sender, RoutedEventArgs e)
+        private void ZoomToScale_Click(object sender, RoutedEventArgs e)
         {
-            Enum.TryParse(((Button)sender).Name, out PanDirection direction);
-            mapView.Pan(direction, 20);
+            mapView.ZoomToScale(Convert.ToDouble(zoomScale.Text));
+        }
+
+        private void LayerExtent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FeatureExtent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PanToLatLon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PanToFeature_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
