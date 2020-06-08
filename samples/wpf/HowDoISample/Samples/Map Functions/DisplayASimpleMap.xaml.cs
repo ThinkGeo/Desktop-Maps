26906f178c12d85b2f7ba2c4e9a68b420efaa089
jackli-tg <jackli@thinkgeo.com>
2020-06-08 09:46:43 -0500

    thinkgeo/public/above-the-fold#244 Updated samples to new maps

--- samples/wpf/HowDoISample/Samples/Map Functions/DisplayASimpleMap.xaml.cs
+++ samples/wpf/HowDoISample/Samples/Map Functions/DisplayASimpleMap.xaml.cs
@@ -19,19 +19,6 @@ namespace ThinkGeo.UI.Wpf.HowDoI
             ThinkGeoCloudVectorMapsOverlay thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay(SampleHelper.ThinkGeoCloudId, SampleHelper.ThinkGeoCloudSecret,ThinkGeoCloudVectorMapsMapType.Light);
             Map1.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
 
-
-            //LayerOverlay utahWaterOverlay = new LayerOverlay() { TileType = TileType.MultiTile };
-            //utahWaterOverlay.Layers.Add(new BackgroundLayer(new GeoSolidBrush(GeoColor.FromArgb(255, 233, 232, 214))));
-
-            //ShapeFileFeatureLayer utahWaterShapeLayer = new ShapeFileFeatureLayer(SampleHelper.Get("UtahWater.shp"));
-            //utahWaterShapeLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyle.CreateSimpleAreaStyle(GeoColor.FromArgb(255, 136, 162, 227));
-            //utahWaterShapeLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
-
-            //utahWaterOverlay.Layers.Add("UtahWaterShapes", utahWaterShapeLayer);
-            //Map1.Overlays.Add("UtahWaterOverlay", utahWaterOverlay);
-
-            //Map1.CurrentExtent = new RectangleShape(-113.11473388671875, 41.949816894531253, -111.08226318359375, 40.499621582031253);
-
             Map1.Refresh();
         }
     }
