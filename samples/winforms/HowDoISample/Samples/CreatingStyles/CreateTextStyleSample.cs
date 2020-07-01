77a01ed98b9f787a539fc98e5bcba6736b8a1aeb
David Rehagen <davidrehagen@thinkgeo.com>
2020-07-01 09:15:00 -0500

    thinkgeo/public/above-the-fold#366
    Set the background color

--- samples/winforms/HowDoISample/Samples/CreatingStyles/CreateTextStyleSample.cs
+++ samples/winforms/HowDoISample/Samples/CreatingStyles/CreateTextStyleSample.cs
@@ -9,105 +9,105 @@ namespace ThinkGeo.UI.WinForms.HowDoI
 {
     public class CreateTextStyleSample : UserControl
     {
-        //private readonly ShapeFileFeatureLayer hotelsLayer = new ShapeFileFeatureLayer(@"../../../Data/Shapefile/Hotels.shp");
-        //private readonly ShapeFileFeatureLayer streetsLayer = new ShapeFileFeatureLayer(@"../../../Data/Shapefile/Streets.shp");
-        //private readonly ShapeFileFeatureLayer parksLayer = new ShapeFileFeatureLayer(@"../../../Data/Shapefile/Parks.shp");
+        private readonly ShapeFileFeatureLayer hotelsLayer = new ShapeFileFeatureLayer(@"../../../Data/Shapefile/Hotels.shp");
+        private readonly ShapeFileFeatureLayer streetsLayer = new ShapeFileFeatureLayer(@"../../../Data/Shapefile/Streets.shp");
+        private readonly ShapeFileFeatureLayer parksLayer = new ShapeFileFeatureLayer(@"../../../Data/Shapefile/Parks.shp");
 
-        //public CreateTextStyleSample()
-        //{
-        //    InitializeComponent();
-        //}
+        public CreateTextStyleSample()
+        {
+            InitializeComponent();
+        }
 
-        //private void Form_Load(object sender, EventArgs e)
-        //{
-        //    // Set the map's unit of measurement to meters(Spherical Mercator)
-        //    mapView.MapUnit = GeographyUnit.Meter;
+        private void Form_Load(object sender, EventArgs e)
+        {
+            // Set the map's unit of measurement to meters(Spherical Mercator)
+            mapView.MapUnit = GeographyUnit.Meter;
 
-        //    // Set the map background color
-        //    mapView.Background = new SolidColorBrush(Color.FromRgb(232, 232, 232));
+            // Set the map background color            
+            mapView.BackgroundOverlay.BackgroundBrush = new GeoSolidBrush( GeoColor.FromArgb(255, 232, 232, 232));
 
-        //    // Project the layer's data to match the projection of the map
-        //    hotelsLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
-        //    streetsLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
-        //    parksLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
+            // Project the layer's data to match the projection of the map
+            hotelsLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
+            streetsLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
+            parksLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
 
-        //    // Add Styles to the layers
-        //    StyleHotelsLayer();
-        //    StyleStreetsLayer();
-        //    StyleParksLayer();
+            // Add Styles to the layers
+            StyleHotelsLayer();
+            StyleStreetsLayer();
+            StyleParksLayer();
 
-        //    // Add layers to a layerOverlay
-        //    var layerOverlay = new LayerOverlay();
-        //    layerOverlay.Layers.Add(parksLayer);
-        //    layerOverlay.Layers.Add(streetsLayer);
-        //    layerOverlay.Layers.Add(hotelsLayer);
+            // Add layers to a layerOverlay
+            var layerOverlay = new LayerOverlay();
+            layerOverlay.Layers.Add(parksLayer);
+            layerOverlay.Layers.Add(streetsLayer);
+            layerOverlay.Layers.Add(hotelsLayer);
 
-        //    // Add overlay to map
-        //    mapView.Overlays.Add(layerOverlay);
+            // Add overlay to map
+            mapView.Overlays.Add(layerOverlay);
 
-        //    // Set the map extent
-        //    mapView.CurrentExtent = new RectangleShape(-10778329.017082, 3909598.36751101, -10776250.8853871, 3907890.47766975);
+            // Set the map extent
+            mapView.CurrentExtent = new RectangleShape(-10778329.017082, 3909598.36751101, -10776250.8853871, 3907890.47766975);
 
-        //    mapView.Refresh();
-        //}
+            mapView.Refresh();
+        }
 
 
-        ///// <summary>
-        ///// Adds a PointStyle and TextStyle to the Hotels Layer
-        ///// </summary>
-        //private void StyleHotelsLayer()
-        //{
-        //    var pointStyle = new PointStyle(PointSymbolType.Circle, 4, GeoBrushes.Brown, new GeoPen(GeoBrushes.DarkRed, 2));
-        //    var textStyle = new TextStyle("NAME", new GeoFont("Segoe UI", 12, DrawingFontStyles.Bold), GeoBrushes.DarkRed)
-        //    {
-        //        TextPlacement = TextPlacement.Lower,
-        //        YOffsetInPixel = 2,
-        //        HaloPen = new GeoPen(GeoBrushes.White, 2),
-        //        DrawingLevel = DrawingLevel.LabelLevel,
-        //        AllowLineCarriage = true
-        //    };
-        //    hotelsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(pointStyle);
-        //    hotelsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(textStyle);
+        /// <summary>
+        /// Adds a PointStyle and TextStyle to the Hotels Layer
+        /// </summary>
+        private void StyleHotelsLayer()
+        {
+            var pointStyle = new PointStyle(PointSymbolType.Circle, 4, GeoBrushes.Brown, new GeoPen(GeoBrushes.DarkRed, 2));
+            var textStyle = new TextStyle("NAME", new GeoFont("Segoe UI", 12, DrawingFontStyles.Bold), GeoBrushes.DarkRed)
+            {
+                TextPlacement = TextPlacement.Lower,
+                YOffsetInPixel = 2,
+                HaloPen = new GeoPen(GeoBrushes.White, 2),
+                DrawingLevel = DrawingLevel.LabelLevel,
+                AllowLineCarriage = true
+            };
+            hotelsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(pointStyle);
+            hotelsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(textStyle);
 
-        //    hotelsLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
-        //}
+            hotelsLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+        }
 
-        ///// <summary>
-        ///// Adds a LineStyle and TextStyle to the Streets Layer
-        ///// </summary>
-        //private void StyleStreetsLayer()
-        //{
-        //    var lineStyle = new LineStyle(new GeoPen(GeoBrushes.DimGray, 6), new GeoPen(GeoBrushes.WhiteSmoke, 4));
-        //    var textStyle = new TextStyle("FULL_NAME", new GeoFont("Segoe UI", 12, DrawingFontStyles.Bold), GeoBrushes.MidnightBlue)
-        //    {
-        //        SplineType = SplineType.StandardSplining,
-        //        HaloPen = new GeoPen(GeoBrushes.White, 2),
-        //        DrawingLevel = DrawingLevel.LabelLevel
-        //    };
-        //    streetsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(lineStyle);
-        //    streetsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(textStyle);
+        /// <summary>
+        /// Adds a LineStyle and TextStyle to the Streets Layer
+        /// </summary>
+        private void StyleStreetsLayer()
+        {
+            var lineStyle = new LineStyle(new GeoPen(GeoBrushes.DimGray, 6), new GeoPen(GeoBrushes.WhiteSmoke, 4));
+            var textStyle = new TextStyle("FULL_NAME", new GeoFont("Segoe UI", 12, DrawingFontStyles.Bold), GeoBrushes.MidnightBlue)
+            {
+                SplineType = SplineType.StandardSplining,
+                HaloPen = new GeoPen(GeoBrushes.White, 2),
+                DrawingLevel = DrawingLevel.LabelLevel
+            };
+            streetsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(lineStyle);
+            streetsLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(textStyle);
 
-        //    streetsLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
-        //}
+            streetsLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+        }
 
-        ///// <summary>
-        ///// Adds an AreaStyle and TextStyle to the Parks Layer
-        ///// </summary>
-        //private void StyleParksLayer()
-        //{
-        //    var areaStyle = new AreaStyle(GeoPens.DimGray, GeoBrushes.PastelGreen);
-        //    var textStyle = new TextStyle("NAME", new GeoFont("Segoe UI", 12, DrawingFontStyles.Bold), GeoBrushes.DarkGreen)
-        //    {
-        //        FittingPolygon = false,
-        //        HaloPen = new GeoPen(GeoBrushes.White, 2),
-        //        DrawingLevel = DrawingLevel.LabelLevel,
-        //        AllowLineCarriage = true
-        //    };
-        //    parksLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(areaStyle);
-        //    parksLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(textStyle);
+        /// <summary>
+        /// Adds an AreaStyle and TextStyle to the Parks Layer
+        /// </summary>
+        private void StyleParksLayer()
+        {
+            var areaStyle = new AreaStyle(GeoPens.DimGray, GeoBrushes.PastelGreen);
+            var textStyle = new TextStyle("NAME", new GeoFont("Segoe UI", 12, DrawingFontStyles.Bold), GeoBrushes.DarkGreen)
+            {
+                FittingPolygon = false,
+                HaloPen = new GeoPen(GeoBrushes.White, 2),
+                DrawingLevel = DrawingLevel.LabelLevel,
+                AllowLineCarriage = true
+            };
+            parksLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(areaStyle);
+            parksLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(textStyle);
 
-        //    parksLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
-        //}
+            parksLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+        }
 
         #region Component Designer generated code
 
