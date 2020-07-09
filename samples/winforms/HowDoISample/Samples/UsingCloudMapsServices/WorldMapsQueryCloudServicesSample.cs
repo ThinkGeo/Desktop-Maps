e6fcc447864afb4a78e8267f8b1fef0cbeda08ae
JoshuaRehagen <joshrehagen@thinkGeo.com>
2020-07-09 16:38:18 -0500

    thinkgeo/public/above-the-fold#366 Fixed a text box.

--- samples/winforms/HowDoISample/Samples/UsingCloudMapsServices/WorldMapsQueryCloudServicesSample.cs
+++ samples/winforms/HowDoISample/Samples/UsingCloudMapsServices/WorldMapsQueryCloudServicesSample.cs
@@ -64,6 +64,9 @@ namespace ThinkGeo.UI.WinForms.HowDoI
 
             // Run the world maps query
             PerformWorldMapsQuery();
+
+            txtMaximumResults.DataBindings.Add("Text", maxResults, "Value");
+
         }
 
         private async void PerformWorldMapsQuery()
@@ -293,8 +296,9 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             // 
             this.maxResults.Location = new System.Drawing.Point(22, 157);
             this.maxResults.Maximum = 1000;
+            this.maxResults.Minimum = 10;
             this.maxResults.Name = "maxResults";
-            this.maxResults.Size = new System.Drawing.Size(227, 45);
+            this.maxResults.Size = new System.Drawing.Size(227, 56);
             this.maxResults.TabIndex = 6;
             this.maxResults.TickStyle = System.Windows.Forms.TickStyle.None;
             this.maxResults.Value = 100;
@@ -310,7 +314,7 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             "Within"});
             this.cboQueryType.Location = new System.Drawing.Point(164, 94);
             this.cboQueryType.Name = "cboQueryType";
-            this.cboQueryType.Size = new System.Drawing.Size(135, 24);
+            this.cboQueryType.Size = new System.Drawing.Size(135, 28);
             this.cboQueryType.TabIndex = 5;
             this.cboQueryType.Text = "Intersecting";
             // 
@@ -334,7 +338,7 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             "Waterways"});
             this.cboQueryLayer.Location = new System.Drawing.Point(164, 52);
             this.cboQueryLayer.Name = "cboQueryLayer";
-            this.cboQueryLayer.Size = new System.Drawing.Size(135, 24);
+            this.cboQueryLayer.Size = new System.Drawing.Size(135, 28);
             this.cboQueryLayer.TabIndex = 4;
             this.cboQueryLayer.Text = "Buildings";
             // 
@@ -345,7 +349,7 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(18, 134);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.Size = new System.Drawing.Size(148, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Maximum Results:";
             // 
@@ -356,7 +360,7 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(18, 94);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.Size = new System.Drawing.Size(100, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Query Type:";
             // 
@@ -367,7 +371,7 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(18, 56);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.Size = new System.Drawing.Size(106, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Query Layer:";
             // 
@@ -378,7 +382,7 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(18, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 20);
+            this.label1.Size = new System.Drawing.Size(259, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Query World Maps Features";
             // 
