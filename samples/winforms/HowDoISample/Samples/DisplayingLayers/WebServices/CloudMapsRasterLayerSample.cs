7b9bbb2dc26510f0441bf8acf41861dcfba5d149
JoshuaRehagen <joshrehagen@thinkGeo.com>
2020-07-09 11:42:36 -0500

    thinkgeo/public/above-the-fold#366 Added more Cloud Samples

--- samples/winforms/HowDoISample/Samples/DisplayingLayers/WebServices/CloudMapsRasterLayerSample.cs
+++ samples/winforms/HowDoISample/Samples/DisplayingLayers/WebServices/CloudMapsRasterLayerSample.cs
@@ -94,6 +94,7 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
             this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton4.ForeColor = System.Drawing.Color.White;
             this.radioButton4.Location = new System.Drawing.Point(23, 139);
@@ -114,7 +115,6 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(73, 24);
             this.radioButton3.TabIndex = 3;
-            this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Aerial";
             this.radioButton3.UseVisualStyleBackColor = true;
             this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
@@ -128,7 +128,6 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(66, 24);
             this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Dark";
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
@@ -142,7 +141,6 @@ namespace ThinkGeo.UI.WinForms.HowDoI
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(67, 24);
             this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Light";
             this.radioButton1.UseVisualStyleBackColor = true;
             this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
