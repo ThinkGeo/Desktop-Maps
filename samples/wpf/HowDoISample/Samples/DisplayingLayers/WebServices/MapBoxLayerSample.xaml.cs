22e29e279d1416fe6607aa8c314ba69f9845590f
Kyle <kyleday@thinkgeo.com>
2020-11-17 17:33:50 -0600

    thinkgeo/public/thinkgeo-desktop-maps#3
    Add mapbox statictile sample

--- samples/wpf/HowDoISample/Samples/DisplayingLayers/WebServices/MapBoxLayerSample.xaml.cs
+++ samples/wpf/HowDoISample/Samples/DisplayingLayers/WebServices/MapBoxLayerSample.xaml.cs
@@ -8,35 +8,31 @@ using System;
 namespace ThinkGeo.UI.Wpf.HowDoI
 {
     /// <summary>
-    /// Learn how to display a Bing Maps Layer on the map
+    /// Learn how to display a MapBox StaticTiles Layer on the map
     /// </summary>
-    public partial class BingMapLayerSample : UserControl, IDisposable
+    public partial class MapBoxLayerSample : UserControl, IDisposable
     {
-        public BingMapLayerSample()
+        public MapBoxLayerSample()
         {
             InitializeComponent();
         }
 
         /// <summary>
-        /// Add the Bing Maps layer to the map
+        /// Add the MapBox StaticTiles layer to the map
         /// </summary>
         private void btnActivate_Click(object sender, RoutedEventArgs e)
         {
             // It is important to set the map unit first to either feet, meters or decimal degrees.
             mapView.MapUnit = GeographyUnit.Meter;
 
-            // Set the map zoom level set to the bing map zoom level set so all the zoom levels line up.
-            mapView.ZoomLevelSet = new BingMapsZoomLevelSet();
-
             // Create the layer overlay with some additional settings and add to the map.
             LayerOverlay layerOverlay = new LayerOverlay() { TileHeight = 256, TileWidth = 256 };
             layerOverlay.TileSizeMode = TileSizeMode.Small;
-            layerOverlay.MaxExtent = MaxExtents.BingMaps;
             mapView.Overlays.Add("Bing Map", layerOverlay);
 
-            // Create the bing map layer and add it to the map.
-            BingMapsLayer bingMapsLayer = new BingMapsLayer(txtApplicationID.Text, BingMapsMapType.Road);
-            layerOverlay.Layers.Add(bingMapsLayer);
+            // Create the MapBox StaticTiles layer and add it to the map.
+            MapBoxStaticTilesLayer mapBoxStaticTilesLayer = new MapBoxStaticTilesLayer(txtAccessToken.Text, MapBoxStyleId.Streets);
+            layerOverlay.Layers.Add(mapBoxStaticTilesLayer);
 
             // Set the current extent to the whole world.
             mapView.CurrentExtent = new RectangleShape(-10000000, 10000000, 10000000, -10000000);
