aad08b177b209deeb5a51195546d2b04e778dd4e
LeoLiu <leoliu@thinkgeo.com>
2022-09-07 22:24:31 +0800

    thinkgeo/products/thinkgeosdk#91 fix the issue that BingMapsLayer could be added multiple times in DisplayABingMapsLayer Sample

--- samples/wpf/HowDoISample/Samples/DisplayingLayers/WebServices/BingMapLayerSample.xaml.cs
+++ samples/wpf/HowDoISample/Samples/DisplayingLayers/WebServices/BingMapLayerSample.xaml.cs
@@ -17,32 +17,39 @@ namespace ThinkGeo.UI.Wpf.HowDoI
             InitializeComponent();
         }
 
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // It is important to set the map unit first to either feet, meters or decimal degrees.
+            mapView.MapUnit = GeographyUnit.Meter;
+
+            // Set the current extent to the whole world.
+            mapView.CurrentExtent = new RectangleShape(-10000000, 10000000, 10000000, -10000000);
+        }
+
         /// <summary>
         /// Add the Bing Maps layer to the map
         /// </summary>
         private void btnActivate_Click(object sender, RoutedEventArgs e)
         {
-            // It is important to set the map unit first to either feet, meters or decimal degrees.
-            mapView.MapUnit = GeographyUnit.Meter;
+            if (txtApplicationID.Text != null && !mapView.Overlays.Contains("Bing Map"))
+            {
+                btnActivate.IsEnabled = false;
+                // Set the map zoom level set to the bing map zoom level set so all the zoom levels line up.
+                mapView.ZoomLevelSet = new BingMapsZoomLevelSet(256);
 
-            // Set the map zoom level set to the bing map zoom level set so all the zoom levels line up.
-            mapView.ZoomLevelSet = new BingMapsZoomLevelSet(256);
+                // Create the layer overlay with some additional settings and add to the map.
+                LayerOverlay layerOverlay = new LayerOverlay() { TileHeight = 256, TileWidth = 256 };
+                layerOverlay.TileSizeMode = TileSizeMode.Small;
+                layerOverlay.MaxExtent = MaxExtents.BingMaps;
+                mapView.Overlays.Add("Bing Map", layerOverlay);
 
-            // Create the layer overlay with some additional settings and add to the map.
-            LayerOverlay layerOverlay = new LayerOverlay() { TileHeight = 256, TileWidth = 256 };
-            layerOverlay.TileSizeMode = TileSizeMode.Small;
-            layerOverlay.MaxExtent = MaxExtents.BingMaps;
-            mapView.Overlays.Add("Bing Map", layerOverlay);
+                // Create the bing map layer and add it to the map.
+                BingMapsLayer bingMapsLayer = new BingMapsLayer(txtApplicationID.Text, BingMapsMapType.Road, "C:\\temp");
+                layerOverlay.Layers.Add(bingMapsLayer);
 
-            // Create the bing map layer and add it to the map.
-            BingMapsLayer bingMapsLayer = new BingMapsLayer(txtApplicationID.Text, BingMapsMapType.Road, "C:\\temp");
-            layerOverlay.Layers.Add(bingMapsLayer);
-
-            // Set the current extent to the whole world.
-            mapView.CurrentExtent = new RectangleShape(-10000000, 10000000, 10000000, -10000000);
-
-            // Refresh the map.
-            mapView.Refresh();
+                // Refresh the map.
+                mapView.Refresh();
+            }
         }
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
@@ -57,5 +64,6 @@ namespace ThinkGeo.UI.Wpf.HowDoI
             // Suppress finalization.
             GC.SuppressFinalize(this);
         }
+
     }
 }
