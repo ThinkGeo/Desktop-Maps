a84b8dae522c33db38b21d0166df5561aa7b635c
LeoLiu <leoliu@thinkgeo.com>
2022-09-24 11:10:11 +0800

    thinkgeo/products/thinkgeosdk#444 add Kml sample

---
+++ samples/wpf/HowDoISample/Samples/DisplayingLayers/Raster/KmlLayerSample.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ThinkGeo.UI.Wpf;
+using ThinkGeo.Core;
+using System;
+
+namespace ThinkGeo.UI.Wpf.HowDoI
+{
+    /// <summary>
+    /// Learn how to display a GeoTiff Layer on the map
+    /// </summary>
+    public partial class KmlLayerSample : UserControl, IDisposable
+    {
+        public KmlLayerSample()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Add the GeoTiff layer to the map
+        /// </summary>
+        private void MapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // It is important to set the map unit first to either feet, meters or decimal degrees.
+            mapView.MapUnit = GeographyUnit.Meter;
+
+            // Create a new overlay that will hold our new layer and add it to the map.
+            LayerOverlay layerOverlay = new LayerOverlay();
+            mapView.Overlays.Add(layerOverlay);
+
+            // Create the new layer and dd the layer to the overlay we created earlier.
+            KmlFeatureLayer layer = new KmlFeatureLayer("../../../Data/Kml/Frisco.kml");
+            layer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle = PointStyle.CreateSimplePointStyle(PointSymbolType.Diamond, GeoColors.Black, 10);
+            layer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle = LineStyle.CreateSimpleLineStyle(GeoColors.Red, 4, true);
+            layer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyle.CreateSimpleAreaStyle(GeoColors.Blue);
+            layer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            layerOverlay.Layers.Add(layer);
+
+            // Set the map view current extent to a slightly zoomed in area of the image.
+            mapView.CurrentExtent = new RectangleShape(-10777998.2731192, 3913070.41013283, -10774999.3141042, 3911542.86390418);
+
+            // Refresh the map.
+            mapView.Refresh();
+        }
+
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            mapView.Dispose();
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+    }
+}
