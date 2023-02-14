06981859b8bb0538443d3f395bcc6264c31dcf29
Rui Li <ruili@thinkgeo.com>
2023-02-14 11:29:03 +0800

    thinkgeo/public/thinkgeo-desktop-maps#14 Add Desktop VS Sample

---
+++ QuickStartGuides/QuickStartGuide_WPF_VS/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using ThinkGeo.Core;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Add a base map overlay.
+            var cloudVectorBaseMapOverlay = new ThinkGeoCloudVectorMapsOverlay("USlbIyO5uIMja2y0qoM21RRM6NBXUad4hjK3NBD6pD0~", "f6OJsvCDDzmccnevX55nL7nXpPDXXKANe5cN6czVjCH0s8jhpCH-2A~~", ThinkGeoCloudVectorMapsMapType.Light);
+            mapView.Overlays.Add(cloudVectorBaseMapOverlay);
+            mapView.CurrentExtent = MaxExtents.ThinkGeoMaps;
+
+            // Add a shapefile layer with point style.
+            var capitalLayer = new ShapeFileFeatureLayer(@"./AppData/WorldCapitals.shp");
+            var capitalStyle = new PointStyle()
+            {
+                SymbolType = PointSymbolType.Circle,
+                SymbolSize = 8,
+                FillBrush = new GeoSolidBrush(GeoColors.White),
+                OutlinePen = new GeoPen(GeoColors.Black, 2)
+            };
+            capitalLayer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle = capitalStyle;
+            capitalLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            // Set the projection of the capitalLayer to Spherical Mercator
+            capitalLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(4326, 3857);
+
+            // Create an overlay to add the layer to and add that overlay to the map.
+            var customDataOverlay = new LayerOverlay();
+            customDataOverlay.Layers.Add(capitalLayer);
+            mapView.Overlays.Add(customDataOverlay);
+            
+            mapView.Refresh(); 
+        }
+    }
+}
