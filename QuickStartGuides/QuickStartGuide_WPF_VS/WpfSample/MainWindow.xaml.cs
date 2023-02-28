62512bda02c6fac409b521d10fea8cb3d6cbfbd8
Rui Li <ruili@thinkgeo.com>
2023-02-28 16:22:17 +0800

    thinkgeo/public/thinkgeo-desktop-maps#14 Replace the Create_WPF_Project_ScreenShot.gif with the new one Update MainWindow.xaml Update MainWindow.xaml.cs Update WpfSample.csproj

--- QuickStartGuides/QuickStartGuide_WPF_VS/WpfSample/MainWindow.xaml.cs
+++ QuickStartGuides/QuickStartGuide_WPF_VS/WpfSample/MainWindow.xaml.cs
@@ -14,7 +14,8 @@ namespace WpfSample
         }
 
         private void mapView_Loaded(object sender, RoutedEventArgs e)
-        {
+        {            
+            mapView.MapUnit = GeographyUnit.Meter;
             // Add a base map overlay.
             var baseOverlay = new ThinkGeoCloudVectorMapsOverlay("USlbIyO5uIMja2y0qoM21RRM6NBXUad4hjK3NBD6pD0~", "f6OJsvCDDzmccnevX55nL7nXpPDXXKANe5cN6czVjCH0s8jhpCH-2A~~", ThinkGeoCloudVectorMapsMapType.Light);
             // Set up the tile cache for the base overlay 
@@ -23,8 +24,8 @@ namespace WpfSample
             
             mapView.CurrentExtent = MaxExtents.ThinkGeoMaps;
 
-            //Add a shapefile layer with point style.
-            var capitalLayer = new ShapeFileFeatureLayer(@"./AppData/WorldCapitals.shp");
+            // Add a shapefile layer with point style.
+            var capitalLayer = new ShapeFileFeatureLayer(@"..\..\..\AppData\WorldCapitals.shp");
             var capitalStyle = new PointStyle()
             {
                 SymbolType = PointSymbolType.Circle,
@@ -32,12 +33,15 @@ namespace WpfSample
                 FillBrush = new GeoSolidBrush(GeoColors.White),
                 OutlinePen = new GeoPen(GeoColors.Black, 2)
             };
+            // Have set the default to have 20 zoom levels in the layer and applied the style to zoomlevel01, which can also be applied to other zoomlevels.
             capitalLayer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle = capitalStyle;
             capitalLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
-            //Set the projection of the capitalLayer to Spherical Mercator
-            capitalLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(4326, 3857);
 
-            //Create an overlay to add the layer to and add that overlay to the map.
+            // The SHP file uses the Decimal Degrees projection, while the map uses the Spherical Mercator projection.
+            // To ensure compatibility, it is necessary to convert the SHP files from Decimal Degrees projection to the map's Spherical Mercator projection.
+            capitalLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(Projection.GetDecimalDegreesProjString(), Projection.GetSphericalMercatorProjString());
+
+            // Create an overlay to add the layer to and add that overlay to the map.
             var customDataOverlay = new LayerOverlay();
             customDataOverlay.Layers.Add(capitalLayer);
             mapView.Overlays.Add(customDataOverlay);
