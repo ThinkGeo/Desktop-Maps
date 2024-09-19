4a8ed59bf9d57e4e316c70570dfa6de3146f7e70
Rui Li <ruili@thinkgeo.com>
2024-09-19 11:29:48 +0800

    thinkgeo/products/thinkgeosdk#740 Add AnimatedMarker Sample

---
+++ samples/wpf/HowDoISample/Samples/MarkersandPopups/CustomIcon.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace ThinkGeo.UI.Wpf.HowDoI
+{
+    /// <summary>
+    /// Interaction logic for CustomIcon.xaml
+    /// </summary>
+    public partial class CustomIcon : INotifyPropertyChanged
+    {
+        private bool _animationStarted;
+        public CustomIcon()
+        {
+            InitializeComponent();
+        }
+
+        public bool AnimationStarted
+        {
+            get => _animationStarted;
+            set
+            {
+                if (_animationStarted != value)
+                {
+                    _animationStarted = value;
+                    OnPropertyChanged(nameof(AnimationStarted));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
