e16ef0eaf2ac23fbfd3ca6286d80a19a74ae2c2e
Rui Li <ruili@thinkgeo.com>
2024-10-24 22:23:38 +0800

    thinkgeo/products/thinkgeosdk#748 Add "Use WmsLayer with ReProjection" Sample

--- samples/wpf/HowDoISample/Samples/MapOnlineData/WMS.xaml.cs
+++ samples/wpf/HowDoISample/Samples/MapOnlineData/WMS.xaml.cs
@@ -20,16 +20,7 @@ namespace ThinkGeo.UI.Wpf.HowDoI
         /// </summary>
         private async void MapView_Loaded(object sender, RoutedEventArgs e)
         {
-            // It is important to set the map unit first to either feet, meters or decimal degrees.
-            MapView.MapUnit = GeographyUnit.DecimalDegree;
-
-            // This code sets up the sample to use the overlay versus the layer.
-            UseOverlay();
-
-            // Set the current extent to a local area.
-            MapView.CurrentExtent = new RectangleShape(-96.8538765269409, 33.1618647290098, -96.7987487018851, 33.1054126590461);
-
-            // Refresh the map.
+            UseLayerWithReProjection();
             await MapView.RefreshAsync();
         }
 
@@ -46,11 +37,17 @@ namespace ThinkGeo.UI.Wpf.HowDoI
                 case "Use WmsRasterLayer":
                     UseLayer();
                     break;
+                case "Use WmsLayer with ReProjection":
+                    UseLayerWithReProjection();
+                    break;
             }
             await MapView.RefreshAsync();
         }
+
         private void UseOverlay()
         {
+            MapView.MapUnit = GeographyUnit.DecimalDegree;
+
             // Clear out the overlays so we start fresh
             MapView.Overlays.Clear();
 
@@ -65,10 +62,15 @@ namespace ThinkGeo.UI.Wpf.HowDoI
 
             // Add the overlay to the map.
             MapView.Overlays.Add(wmsOverlay);
+
+            // Set the current extent to a local area.
+            MapView.CurrentExtent = new RectangleShape(-96.8538765269409, 33.1618647290098, -96.7987487018851, 33.1054126590461);
         }
 
         private void UseLayer()
         {
+            MapView.MapUnit = GeographyUnit.DecimalDegree;
+
             // Clear out the overlays so we start fresh
             MapView.Overlays.Clear();
 
@@ -85,7 +87,54 @@ namespace ThinkGeo.UI.Wpf.HowDoI
 
             // Add the layer to the overlay.
             staticOverlay.Layers.Add("wmsImageLayer", wmsImageLayer);
+
+            // Set the current extent to a local area.
+            MapView.CurrentExtent = new RectangleShape(-96.8538765269409, 33.1618647290098, -96.7987487018851, 33.1054126590461);
         }
+
+        private void UseLayerWithReProjection()
+        {
+            MapView.MapUnit = GeographyUnit.Meter;
+            // Clear out the overlays so we start fresh
+            MapView.Overlays.Clear();
+
+            // Create an overlay that we will add the layer to.
+            var staticOverlay = new LayerOverlay();
+            MapView.Overlays.Add(staticOverlay);
+
+            // Create a ProjectionConverter to convert from EPSG:4326 (WGS 84) to EPSG:3857 (Spherical Mercator).
+            var gdalProjectionConverter = new GdalProjectionConverter(4326, 3857);
+            // Open the projection converter before using it
+            if (!gdalProjectionConverter.IsOpen)
+            {
+                gdalProjectionConverter.Open();
+            }
+
+            // Create the first WMS layer using the parameters below.
+            var wmsImageLayer = new Core.WmsAsyncLayer(new Uri("http://ows.mundialis.de/services/service"));
+            wmsImageLayer.ActiveLayerNames.Add("OSM-WMS");
+            wmsImageLayer.ActiveStyleNames.Add("default");
+            wmsImageLayer.Exceptions = "application/vnd.ogc.se_xml";
+            wmsImageLayer.Transparency = 100;
+
+            // Apply the projection conversion to WMS layer (convert from EPSG:4326 to EPSG:3857)
+            wmsImageLayer.ProjectionConverter = gdalProjectionConverter;
+            // Add the layer to the overlay.
+            staticOverlay.Layers.Add("wmsImageLayer", wmsImageLayer);
+
+            // Create the second WMS layer using the parameters below.
+            var wmsLayer = new WmsAsyncLayer(new Uri("http://geo.vliz.be/geoserver/Dataportal/ows?service=WMS&"));
+            wmsLayer.DrawingExceptionMode = DrawingExceptionMode.DrawException;
+            wmsLayer.Parameters.Add("LAYERS", "eurobis_grid_15m-obisenv");  
+            wmsLayer.Parameters.Add("STYLES", "generic");  
+            wmsLayer.OutputFormat = "image/png";  
+            wmsLayer.Crs = "EPSG:3857";  // Coordinate system, typically EPSG:3857 for WMS with Spherical Mercator
+            wmsLayer.Transparency = 100;
+
+            // Set the map's current extent
+            MapView.CurrentExtent = new RectangleShape(14702448.140544016, -1074476.17351944, 15302448.801711123, -5574476.985662017);
+            staticOverlay.Layers.Add(wmsLayer);
+        }       
 
         public void Dispose()
         {
