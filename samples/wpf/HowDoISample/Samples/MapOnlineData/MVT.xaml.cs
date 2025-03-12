cc74a662b16ffe2295d84401a83598e9aa0c332e
Leo Liu <leoliu@thinkgeo.com>
2025-03-12 23:08:51 +0800

    thinkgeo/products/thinkgeosdk#785 Delete MVT sample (reverted from commit f8153a343f8591e90a3ffb916df7d17668980e64)

---
+++ samples/wpf/HowDoISample/Samples/MapOnlineData/MVT.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using ThinkGeo.Core;
+
+namespace ThinkGeo.UI.Wpf.HowDoI
+{
+    /// <summary>
+    /// Interaction logic for MVT.xaml
+    /// </summary>
+    public partial class MVT
+    {
+        public MVT()
+        {
+            InitializeComponent();
+        }
+
+        private async void MapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MapView.MapUnit = GeographyUnit.Meter;
+                LayerOverlay _layerOverlay = new LayerOverlay();
+                MapView.Overlays.Add(_layerOverlay);
+
+                var scaleFactor = (float)PresentationSource.FromVisual(Application.Current.MainWindow).CompositionTarget.TransformToDevice.M11;
+                var openstackMbtiles = new MvtTilesAsyncLayer("https://tiles.preludemaps.com/styles/TG_Savannah_Light/style.json");
+                openstackMbtiles.MaxZoomOfTheData = 14;
+
+                _layerOverlay.Layers.Add(openstackMbtiles);
+                await openstackMbtiles.OpenAsync();
+                MapView.CurrentExtent = new RectangleShape(-14377, 6712202, -12504, 6710862);
+
+                await MapView.RefreshAsync();
+            }
+            catch 
+            {
+                // Because async void methods don’t return a Task, unhandled exceptions cannot be awaited or caught from outside.
+                // Therefore, it’s good practice to catch and handle (or log) all exceptions within these “fire-and-forget” methods.
+            }
+        }
+    }
+}
