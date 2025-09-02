f9437f28c3c6e1a7e44914f9938547e7dcb1fc16
Rui Li <ruili@thinkgeo.com>
2025-09-02 15:16:56 +0800

    thinkgeo/products/thinkgeosdk#851 Update Wpf WFS sample

--- samples/wpf/HowDoISample/Samples/MapOnlineData/WFS.xaml.cs
+++ samples/wpf/HowDoISample/Samples/MapOnlineData/WFS.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using ThinkGeo.Core;
 
 namespace ThinkGeo.UI.Wpf.HowDoI
 {
-    public partial class WFS
+    public partial class WFS : IDisposable
     {
         public WFS()
         {
@@ -27,24 +29,130 @@ namespace ThinkGeo.UI.Wpf.HowDoI
             };
             MapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
 
-            var helsinkiParcelsLayer = new WfsV2FeatureLayer("https://inspire-wfs.maanmittauslaitos.fi/inspire-wfs/cp/ows", "cp:CadastralParcel")
-            {
-                TimeoutInSeconds = 500,
-            };
-            helsinkiParcelsLayer.ZoomLevelSet.ZoomLevel13.DefaultAreaStyle = AreaStyle.CreateSimpleAreaStyle(GeoColors.Transparent, GeoColors.OrangeRed, 4);
-            helsinkiParcelsLayer.ZoomLevelSet.ZoomLevel13.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
-            helsinkiParcelsLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(3067, 3857);
+            var helsinkiParcelsLayer = CreateHelsinkiParcelsLayer();
 
-            var overlay = new WfsV2Overlay()
-            {
-                FeatureLayer = helsinkiParcelsLayer,
-                DrawingBulkCount = 500
-            };
+            var layerOverlay = new LayerOverlay { TileType = TileType.SingleTile };
+            layerOverlay.Layers.Add(helsinkiParcelsLayer);
+            MapView.Overlays.Add(layerOverlay);
 
             MapView.CenterPoint = new PointShape(2777730, 8435220);
             MapView.CurrentScale = 20520;
-            MapView.Overlays.Add("LayerOverlay", overlay);
+
             _ = MapView.RefreshAsync();
+        }
+
+        private WfsV2FeatureLayer CreateHelsinkiParcelsLayer()
+        {
+            var layer = new WfsV2FeatureLayer(
+                "https://inspire-wfs.maanmittauslaitos.fi/inspire-wfs/cp/ows",
+                "cp:CadastralParcel")
+            {
+                TimeoutInSeconds = 500
+            };
+
+            layer.ZoomLevelSet.ZoomLevel13.DefaultAreaStyle =
+                AreaStyle.CreateSimpleAreaStyle(GeoColors.Transparent, GeoColors.OrangeRed, 4);
+            layer.ZoomLevelSet.ZoomLevel13.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            layer.FeatureSource.ProjectionConverter = new ProjectionConverter(3067, 3857);
+
+            // Attach event handlers here so they’re always included
+            layer.SendingWebRequest += HelsinkiParcelsLayer_SendingWebRequest;
+
+            var featureSource = (WfsV2FeatureSource)layer.FeatureSource;
+            featureSource.RequestingData += WFS_RequestingData;
+
+            return layer;
+        }
+
+        private void OverlayType_OnCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is RadioButton radio) || radio.IsChecked == false)
+                return;
+
+            // Keep background overlays
+            var backgroundOverlays = MapView.Overlays
+                                            .Where(o => o.Name != "WfsOverlay" && o.Name != "LayerOverlay")
+                                            .ToList();
+            MapView.Overlays.Clear();
+            foreach (var overlay in backgroundOverlays)
+                MapView.Overlays.Add(overlay);
+
+            switch (radio.Content.ToString())
+            {
+                case "WfsV2Overlay":
+                    AddWfsV2Overlay();
+                    break;
+
+                case "LayerOverlay":
+                    AddLayerOverlay();
+                    break;
+            }
+
+            _ = MapView.RefreshAsync();
+        }
+
+
+        private void AddLayerOverlay()
+        {
+            // Try to get an existing overlay with the key
+            LayerOverlay overlay;
+            if (MapView.Overlays.Contains("LayerOverlay"))
+            {
+                overlay = MapView.Overlays["LayerOverlay"] as LayerOverlay;
+                overlay.Layers.Clear(); // clear existing layers
+            }
+            else
+            {
+                overlay = new LayerOverlay { TileType = TileType.SingleTile };
+            }
+
+            // Add the WFS layer
+            var helsinkiParcelsLayer = CreateHelsinkiParcelsLayer();
+            overlay.Layers.Add(helsinkiParcelsLayer);
+
+            // If it’s a new overlay, add to MapView
+            if (!MapView.Overlays.Contains("LayerOverlay"))
+            {
+                MapView.Overlays.Add("LayerOverlay", overlay);
+            }
+        }
+
+        private void AddWfsV2Overlay()
+        {
+            // Try to get an existing overlay with the key
+            WfsV2Overlay overlay;
+            if (MapView.Overlays.Contains("WfsOverlay"))
+            {
+                overlay = MapView.Overlays["WfsOverlay"] as WfsV2Overlay;
+                overlay.FeatureLayer = null; // clear existing layer
+            }
+            else
+            {
+                overlay = new WfsV2Overlay()
+                {
+                    DrawingBulkCount = 500
+                };
+            }
+            
+            // Add the WFS layer
+            var helsinkiParcelsLayer = CreateHelsinkiParcelsLayer();
+            overlay.FeatureLayer = helsinkiParcelsLayer;
+
+            // If it’s a new overlay, add to MapView
+            if (!MapView.Overlays.Contains("WfsOverlay"))
+            {
+                MapView.Overlays.Add("WfsOverlay", overlay);
+            }
+        }
+
+        private void HelsinkiParcelsLayer_SendingWebRequest(object sender, SendingWebRequestEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"[WFS v1] Request: {e.WebRequest.RequestUri}");
+        }
+
+        private void WFS_RequestingData(object sender, RequestingDataWfsFeatureSourceEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"[WFS v2] Request: {e.ServiceUrl}");
         }
 
         public void Dispose()
